1c43ca13209cef7b06fe64a1151eddb092f52001
Thisara Medagoda <thisara.medagoda@dialog.lk>
2025-06-14 16:48:01 +0530

    initial commit.

---
+++ src/main/java/com/financial/tracker/FinancialTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.financial.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financial/tracker/FinancialTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
